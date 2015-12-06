9943d762dca38532b690678be835f5bee7e87664
Alex McVittie <mcvittal@hotmail.com>
2015-12-06 01:48:33 -0500

    Added example information to readme

--- qpy.py
+++ qpy.py
@@ -1,9 +1,3 @@
-###
-###
-### You are free to modify this script to fit your needs.
-### 
-###
-
 #!/usr/bin/python
 from modules.processing_obj import Qprocess
 from modules.processing_functions import *
