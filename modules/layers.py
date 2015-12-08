9270570249a49e22ba17fe3f5c6b7c380e950329
Alex McVittie <mcvittal@hotmail.com>
2015-12-08 17:45:41 -0500

    Input datatype specified

--- modules/layers.py
+++ modules/layers.py
@@ -5,8 +5,10 @@ qgpy = Qprocess()
 
 p = qgpy.getp()
 
+# create_shp: String -> None
 # Easily create a QGIS vector shapefile layer.
 # Sets name to be the filename without extension.
+
 def create_shp(shpfile_path):
 	return qgc.QgsVectorLayer(shpfile_path, shpfile_path[:-4], "qpy")
 
