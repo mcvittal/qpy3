4ec9a02befc501dd1657155f918545c392487d66
Alex McVittie <mcvittal@hotmail.com>
2015-12-11 17:24:02 -0500

    Bug fix
    Having a class two folders deep caused a segfault error, rendering live console typing impossible. Will look into a proper fix in the future.

--- qpy.py
+++ qpy.py
@@ -2,7 +2,7 @@
 from modules.processing_obj import Qprocess
 from modules.processing_functions import *
 from modules.layers import *
-from modules.easy_processing.vector_toolsetA import *
+from modules.vector_toolsetA import *
 
 
 
