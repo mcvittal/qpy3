cbd87d08118f986f70c84ac99cff8f5d285e0e80
mcvittal <alexander.mcvittie@gmail.com>
2016-10-03 16:59:03 -0400

    Continued work on a raster layer function

--- modules/layers.py
+++ modules/layers.py
@@ -2,6 +2,7 @@
 
 import sys
 from processing_obj import Qprocess
+from qgis.core import QgsRasterLayer
 import qgis.core as qgc
 
 qgpy = Qprocess()
@@ -24,15 +25,11 @@ def create_shp(shpfile_path):
 
 # create_raster: String -> QgsRasterLayer
 # Easily create a QGIS raster layer.
-# Sets name of raster to be filename without extension 
 def create_raster(raster_path):
 	fileinfo = QFileInfo(raster_path)
 	baseName = fileInfo.baseName()
-	
-	lyr = qgc.QgsRasterLayer(raster_path, raster_path[:-4])
-	if not lyr.isValid():
-		print "Invalid layer. Bailing out, good luck."
-		sys.exit(0)
-	else:
-		return lyr 
+	path = fileInfo.filePath()
+	if (baseName and path):
+		raster = QgsRasterLayer(path, baseName)
+	return raster
 
