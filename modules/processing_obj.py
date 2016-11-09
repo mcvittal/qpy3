d8c1dcca4ab7ce4c4c250e75d2a09841a3d1ed6c
mcvittal <alexander.mcvittie@gmail.com>
2016-11-09 17:30:45 -0500

    Fixed core processing logic

--- modules/processing_obj.py
+++ modules/processing_obj.py
@@ -1,10 +1,24 @@
-#!/usr/bin/env python
-#   Prepare the environment
+#!/usr/bin/env python2
+
 import sys, os, platform
 
-# Determine what OS is being used
-this_os = platform.platform().lower()
-qgisprefix = ""
+
+# Creates a dummy QGIS interface for the processing objects to interact with
+class DummyInterface(object):
+	def __init__(self):
+		self.destCrs = None
+	def __getattr__(self, *args, **kwargs):
+		def dummy(*args, **kwargs):
+			return DummyInterface()
+		return dummy
+	def __iter__(self):
+		return self
+	def next(self):
+		raise StopIteration
+	def layers(self):
+		# simulate iface.legendInterface().layers()
+		return qgis.core.QgsMapLayerRegistry.instance().mapLayers().values()
+	
 
 def get_qgisprefix():
 	this_os = platform.platform().lower()
@@ -26,14 +40,11 @@ def get_qgisprefix():
 			qgis_folder = ""
 			for folder in os.listdir(r"C:\Program Files (x86)"):
 				if "QGIS" in folder.upper():
-					qgis_folder = os.path.join(r"C:\Program Files (x86)",
-											   folder)
-					break
-			if qgis_folder == "":
+					return os.path.join(r"C:\Program Files (x86)", folder)
+			if qgis_folder == "" or qgis_folder == None:
 				for folder in os.listdir(r"C:\Program Files"):
 					if "QGIS" in folder.upper():
-						qgis_folder = os.path.join(r"C:\Program Files", folder)
-						break
+						return os.path.join(r"C:\Program Files", folder)
 			if qgis_folder == "":
 				print("Could not find QGIS installation.")
 				print("Please email alexander.mcvittie@gmail.com for assistance")
@@ -43,8 +54,7 @@ def get_qgisprefix():
 			qgis_folder = ""
 			for folder in os.listdir(r"C:\Program Files"):
 				if "QGIS" in folder.upper():
-					qgis_folder = os.path.join(r"C:\Program Files", folder)
-					break
+					return os.path.join(r"C:\Program Files", folder)
 				
 			# Location of QGIS installation has been found. 
 			# If it hasnt been found yet, it means it's not installed or they have
@@ -62,148 +72,57 @@ def get_qgisprefix():
 				return os.path.join(qgis_base, "apps", "qgis")
 	else:
 		return "/usr"
-
-
-
-
-if "windows" in this_os:
-	print "Initializing for a Windows system"
-	qgis_folder = None
-	is_osgeo = True
-	
-	if os.path.isdir(r"C:\OSGeo4W\apps\qgis\python\plugins"):
-		sys.path.append(r"C:\OSGeo4W\apps\qgis\python\plugins")
-		qgisprefix = r"C:\OSGeo4W\apps\qgis"
-	elif os.path.isdir(r"C:\OSGeo4W\apps\qgis-ltr\python\plugins"):
-		sys.path.append(r"C:\OSGeo4W\apps\qgis-ltr\python\plugins")
-		qgisprefix = r"C:\OSGeo4W\apps\qgis-ltr"
-	elif os.path.isdir(r"C:\OSGeo4W64\apps\qgis\python\plugins"):
-		sys.path.append(r"C:\OSGeo4W64\apps\qgis\python\plugins")
-		qgisprefix = r"C:\OSGeo4W64\apps\qgis"
-	elif os.path.isdir(r"C:\OSGeo4W64\apps\qgis-ltr\python\plugins"):
-		sys.path.append(r"C:\OSGeo4W64\apps\qgis-ltr\python\plugins")
-		qgisprefix = r"C:\OSGeo4W64\apps\qgis-ltr"
-	elif os.path.isdir(r"C:\Program Files (x86)"):
-		is_osgeo = False
-		qgis_folder = ""
-		for folder in os.listdir(r"C:\Program Files (x86)"):
-			if "QGIS" in folder.upper():
-				qgis_folder = os.path.join(r"C:\Program Files (x86)",
-										   folder)
-				break
-		if qgis_folder == "":
-			for folder in os.listdir(r"C:\Program Files"):
-				if "QGIS" in folder.upper():
-					qgis_folder = os.path.join(r"C:\Program Files", folder)
-					break
-		if qgis_folder == "":
-			print("Could not find QGIS installation.")
-			print("Please email alexander.mcvittie@gmail.com for assistance")
-			sys.exit(0)
+def init_path():
+	this_os = platform.platform().lower()
+	prefix = get_qgisprefix()
+	if "windows" in this_os:
+		qgis_subdir = "qgis"
+		if os.path.isdir(os.path.join(prefix, r"apps\qgis-ltr")):
+			qgis_subdir = "qgis-ltr"
+			
+		sys.path.append(os.path.join(prefix, "apps", qgis_subdir, "python"))
+		sys.path.append(os.path.join(prefix, "apps", qgis_subdir, r"python\plugins"))
+		sys.path.append(os.path.join(prefix, "apps"))
+		sys.path.append(os.path.join(prefix, "bin"))
+		return os.path.join(prefix, "apps", qgis_subdir)
+		
 	else:
-		is_osgeo = False
-		qgis_folder = ""
-		for folder in os.listdir(r"C:\Program Files"):
-			if "QGIS" in folder.upper():
-				qgis_folder = os.path.join(r"C:\Program Files", folder)
-				break
-            
-        # Location of QGIS installation has been found. 
-        # If it hasnt been found yet, it means it's not installed or they have
-        # a custom setup. If the latter is the case, I'll need to add an
-        # exception to the code.
-        if qgis_folder == "":
-            print("Could not find QGIS installation.")
-            print("Please email alexander.mcvittie@gmail.com for assistance")
-            sys.exit(0)
-        qgis_base = qgis_folder
+		os.environ['PATH'] = qgisprefix + '/bin'
+		os.environ['LD_LIBRARY_PATH'] = qgisprefix+'/lib'
+		#Allows for stderr and stdout to be printed to screen - QGIS swallows it otherwise 
+		sys.path.insert(0, qgisprefix+'/share/qgis/python')
+		sys.path.insert(1, qgisprefix+'/share/qgis/python/plugins')
+		return "/usr"
 
-        if not is_osgeo and "qgis-ltr" in os.listdir(os.path.join(qgis_base, "apps")):
-            qgisprefix = os.path.join(qgis_base, "apps", "qgis-ltr")
-            sys.path.append(os.path.join(qgis_base, "apps", "qgis-ltr", "python", "plugins"))
-        elif not is_osgeo:
-            sys.path.append(os.path.join(qgis_base, "apps", "qgis", "python", "plugins"))
-            qgisprefix = os.path.join(qgis_base, "apps", "qgis")
-else:
-    print "Initializing qpy for a Linux system"
-    # Setup path to processing modules
-    qgisprefix = '/usr'
-    # Set up the environment path settings, append QGIS library locations 
-    os.environ['PATH'] = qgisprefix + '/bin'
-    os.environ['LD_LIBRARY_PATH'] = qgisprefix+'/lib'
-    #Allows for stderr and stdout to be printed to screen - QGIS swallows it otherwise 
-    sys.path.insert(0, qgisprefix+'/share/qgis/python')
-    sys.path.insert(1, qgisprefix+'/share/qgis/python/plugins')
-    #QgsApplication.setPrefixPath(qgisprefix, True)
+		
+# Configure the stuff that differs between operating systems.
+qgsapplication_prefix = init_path()
 
-#uninstallErrorHook() # Needed it for earlier versions of QGIS 
+# Standard configuration
 os.environ['QGIS_DEBUG'] = '-1'
 
-#Package imports
-import qgis
+# Imports
 from qgis.core import *
 from qgis.gui import *
-from PyQt4.QtGui import QApplication
-
-app = QApplication([])
-QgsApplication.setPrefixPath(qgisprefix, True)
-QgsApplication.initQgis()
-from PyQt4 import QtCore, QtGui
-import processing 
-from processing.core.Processing import Processing
-from processing.tools import general as g
 
 
-# Creates a dummy QGIS interface for the processing objects to interact with
-class DummyInterface(object):
-    def __init__(self):
-        self.destCrs = None
-    def __getattr__(self, *args, **kwargs):
-        def dummy(*args, **kwargs):
-            return DummyInterface()
-        return dummy
-    def __iter__(self):
-        return self
-    def next(self):
-        raise StopIteration
-    def layers(self):
-        # simulate iface.legendInterface().layers()
-        return qgis.core.QgsMapLayerRegistry.instance().mapLayers().values()
+def init_qprocess():
+	QgsApplication.setPrefixPath(qgsapplication_prefix, True)
+	app = QgsApplication([], True)
+	QgsApplication.initQgis()
+	from PyQt4 import QtCore, QtGui
+	import processing
+	from processing.core.Processing import Processing
+	from processing.tools import general as g
 
-# The class that gets created in all subsequent qpy modules.
-#     Creates a processing object and allows other .py classes
-#     and methods to obtain it.
+	# Create a dummy QGIS interface. Requires an X server connection in linux to run.
+	iface = DummyInterface()
+	plugin = processing.classFactory(iface)
+	Processing.initialize()
+	return g
+
 class Qprocess():
-    QgsApplication.setPrefixPath(get_qgisprefix(), True)
-    iface = DummyInterface()
-    plugin = processing.classFactory(iface)
-    Processing.initialize()
-    def getp(self):
-        return g 
-    def getqgs(self):
-        return QgsApplication
-    def exit(self):
-        try:
-            del app 
-        except:
-            print "Failed deleting app"
-        try:
-            del plugin
-        except:
-            print "Failed deleting plugin"
-        try:
-            del iface
-        except:
-            print "Failed deleting iface"
-        try:
-            del QgsApplication
-        except:
-            print "Failed deleting QgsApplication"
-        try:
-            del g 
-        except:
-            print "Failed deleting processing"
-        try:
-            del self
-        except:
-            print "Failed deleting Qprocess"
+	def __init__(self):
+		pass
+	def getp(self):
+		return init_qprocess()
