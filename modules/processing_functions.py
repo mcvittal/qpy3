d1e25ef11ab154b1ad2f29c6c06421cf1f7c332d
mcvittal <alexander.mcvittie@gmail.com>
2016-11-14 16:59:16 -0500

    Processing framework object is now passed into processing functions
    Requires being called qpy.core.alglist() instead of qpy.alglist() now.

--- modules/processing_functions.py
+++ modules/processing_functions.py
@@ -1,59 +1,57 @@
 #!/usr/bin/env python
 from processing_obj import Qprocess
+class CoreFunctions():
+	def __init__(self, p, qgpy):
+		self.p = p
+		self.gpy = qgpy
+	#runalg: Any -> None
+	# Calls the runalg function from QGIS processing
+	def runalg(self, *arg):
+		arguments = arg
+		self.p.runalg(*arguments)
+
+	#alglist: Any -> None
+	# Calls the alglist function from QGIS processing
+	def alglist(self, *arg):
+		arguments = arg
+		self.p.alglist(*arguments)
+
+	#alghelp: String -> None
+	# Shows help on an algorithm passed in.
+	def alghelp(self, algorithm):
+		self.p.alghelp(algorithm)
+
+	# algoptions: String -> None
+	#  Shows options available for a QGIS algorithm
+	def algoptions(self, algorithm):
+		self.p.algoptions(algorithm)
+
+	# getobj
+	#  returns the specified QGIS object
+	def getobj(self, obj):
+		return self.p.getobject(obj)
+
+	# values
+	#   Returns the values of the specified QGIS object
+	def values(self, *arg):
+		return self.p.values(*arg)
+
+	# getfeatures: QGIS Layer object -> ??
+	#  Returns the features of the specified QGIS layer
+	def getfeatures(self, layer):
+		return self.p.getfeatures(layer)
 
 
-qgpy = Qprocess()
-#Processing object
-p = qgpy.getp()
+	# uniquelabels: Any -> Any
+	#  Returns all unique labels for layers in qpy instance 
+	def uniquelabels(self, *arg):
+		return self.p.uniquelabels(*arg)
 
-#runalg: Any -> None
-# Calls the runalg function from QGIS processing
-def runalg(*arg):
-	arguments = arg
-	p.runalg(*arguments)
-
-#alglist: Any -> None
-# Calls the alglist function from QGIS processing
-def alglist(*arg):
-	arguments = arg
-	p.alglist(*arguments)
-
-#alghelp: String -> None
-# Shows help on an algorithm passed in.
-def alghelp(algorithm):
-	p.alghelp(algorithm)
-
-# algoptions: String -> None
-#  Shows options available for a QGIS algorithm
-def algoptions(algorithm):
-	p.algoptions(algorithm)
-
-# getobj
-#  returns the specified QGIS object
-def getobj(obj):
-	return p.getobject(obj)
-
-# values
-#   Returns the values of the specified QGIS object
-def values(*arg):
-	return p.values(*arg)
-
-# getfeatures: QGIS Layer object -> ??
-#  Returns the features of the specified QGIS layer
-def getfeatures(layer):
-	return p.getfeatures(layer)
-
-
-# uniquelabels: Any -> Any
-#  Returns all unique labels for layers in qpy instance 
-def uniquelabels(*arg):
-	return p.uniquelabels(*arg)
-
-# close: None -> None
-# Closes the connection to QgsApplication. Haven't found this to be 
-#  necessary during testing, but its good practice.
-def close():
-	qgpy.exit()
-	del qgpy
+	# close: None -> None
+	# Closes the connection to QgsApplication. Haven't found this to be 
+	#  necessary during testing, but its good practice.
+	def close(self):
+		self.qgpy.exit()
+		del self.qgpy
 
 
--- qpy.py
+++ qpy.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 from modules.processing_obj import Qprocess
-from modules.processing_functions import *
+from modules.processing_functions import CoreFunctions
 from modules.layers import *
-from modules.vector_toolsetA import *
-from modules.raster_toolsetA import *
-from modules.global_variables import *
 
-import modules.network_analysis as network 
+'''
+from modules.vector_toolsetA import VectorA
+from modules.raster_toolsetA import RasterA
+from modules.global_variables import GlobVar
+from modules.network_analysis import NetworkAnalyst
+'''
+# Initialize a Processing framework object, g
+q = Qprocess()
+g = q.getp()
 
+core = CoreFunctions(g, q)
+#vector_basic = VectorA(g)
+#raster_basic = RasterA(g)
+#raster_basic = RasterA(g)
 
 
 
