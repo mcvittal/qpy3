7598ca9e02b734baea9524dd3cc5033cbde1d125
mcvittal <alexander.mcvittie@gmail.com>
2017-01-12 12:21:44 -0500

    Robust batch script to run qpy scripts in windows.

---
+++ win_runqpy.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+
+REM This script is for running Qpy scripts on Windows standalone by forcing it to be run in the OSGeo4W shell.
+
+REM Check if OSGeo4W exists 
+IF NOT EXIST C:\OSGeo4W GOTO PROGRAMFILES
+Set "folder=C:\OSGeo4W"
+GOTO CALLPYTHON
+
+
+:PROGRAMFILES
+for /d %%a in (
+  "C:\Program Files\QGIS*"
+) do (
+  Set "folder=%%~fa"
+)
+
+
+:CALLPYTHON
+
+REM Call the python script passed in
+
+IF NOT EXIST "%folder%\bin\python-qgis-ltr.bat" GOTO NONLTR
+"%folder%\OSGeo4W.bat" "%folder%\bin\python-qgis-ltr.bat" %1
+
+:NONLTR
+"%folder%\OSGeo4W.bat" "%folder%\bin\python-qgis.bat" %1
+
+
