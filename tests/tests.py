e224ad456ab85acd06ff185184b243903ceec3a5
Alex McVittie <alexander.mcvittie@gmail.com>
2019-12-13 01:04:52 -0500

    Addition of testing module

---
+++ tests/tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys, traceback
+
+sys.path.append("..")
+
+from qpy3 import Qpy
+
+print("Testing buffering")
+
+try:
+    Qpy.Buffer("../test_data/point1.shp", "test_output/buffered.shp", 0.5)
+    print("Buffering succeeded")
+except Exception:
+    traceback.print_exc(file=sys.stdout)
