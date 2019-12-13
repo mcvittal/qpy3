f1111a66fd449b05c2c4bc2867e23cd60aa6d08f
Alex McVittie <alexander.mcvittie@gmail.com>
2019-12-13 00:26:17 -0500

    Algorithm list method

--- qpy3.py
+++ qpy3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-## The new home for qpy - qpy2. The old qpy is slated for removal and will be replaced with this
+## The new home for qpy - qpy3. The old qpy is slated for removal and will be replaced with this
 ## class.
 
 ## Written 12/12/2019 by @mcvittal
@@ -22,27 +22,6 @@ Processing.initialize()
 A = Analysis(processing, Processing)
 
 
-
-'''
-processing.algorithmHelp("gdal:buffervectors")
-
-vlayer = QgsVectorLayer("test_data/point1.shp", "Test layer", "ogr")
-params = {}
-params["INPUT"] = vlayer
-params["DISTANCE"] = 1
-params["DISSOLVE"] = True
-params["OUTPUT"] = "/tmp/out.shp"
-#processing.run("gdal:buffervectors", params)
-'''
-
-
-
-
-    #processing.run("qgis:creategrid")
-    #for alg in QgsApplication.processingRegistry().algorithms():
-    #    print("{}:{} --> {}".format(alg.provider().name(), alg.name(), alg.displayName()))
-
-
-
-    #print("unable to load processing")
-    #print("unable to load processing")
+def list_all_algorithms():
+    for alg in QgsApplication.processingRegistry().algorithms():
+        print("{}:{} --> {}".format(alg.provider().name(), alg.name(), alg.displayName()))
