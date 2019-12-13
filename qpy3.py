fbb97407f7a86e0d5decff4dee03f38dd16c4947
Alex McVittie <alex@skywatch.com>
2019-12-13 14:24:51 -0500

    Addition of xvfbwrapper for Linux machines for headless processing

--- qpy3.py
+++ qpy3.py
@@ -6,14 +6,27 @@
 ## Written 12/12/2019 by @mcvittal
 
 from modules.Analysis import Analysis
-
+from xvfbwrapper import Xvfb
 import sys
 from qgis.core import QgsApplication, QgsVectorLayer
+
+
 gui_flag = False
+
+# For headless servers - fake x server so dummy QGIS instance can run 
+vdisplay = Xvfb()
+vdisplay.start()
+
+#Create dummy instance 
 app = QgsApplication([], gui_flag)
+
+# Instantiate the dummy instance 
 QgsApplication.setPrefixPath("/usr", True)
 QgsApplication.initQgis()
+
+# Add the processing plugin to the python path 
 sys.path.append('/usr/share/qgis/python/plugins')
+
 # Load processing package
 from processing.core.Processing import Processing
 import processing
@@ -25,6 +38,7 @@ class Qpy(Analysis):
 Qpy = Qpy(processing, Processing)
 
 
+# Helpful general functions go here 
 def list_all_algorithms():
     for alg in QgsApplication.processingRegistry().algorithms():
         print("{}:{} --> {}".format(alg.provider().name(), alg.name(), alg.displayName()))
