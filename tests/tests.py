6bda03cd5cf0a1f38319ad11109b29e638d41dc1
Alex McVittie <alexander.mcvittie@gmail.com>
2020-10-02 15:23:11 -0400

    Test update, docker file creation

--- tests/tests.py
+++ tests/tests.py
@@ -12,27 +12,27 @@ if not os.path.exists("test_output"):
 
 try:
     print("Testing buffering")
-    Qpy.Buffer("test_data/point1.shp", "test_output/buffered.shp", 0.5)
+    Qpy.buffer_vectors("test_data/point1.shp", "test_output/buffered.shp", 0.5)
     print("Buffering succeeded")
 except Exception:
     traceback.print_exc(file=sys.stdout)
     
 try:
     print("Testing selection")
-    Qpy.Select_analysis("test_data/multiple_polygons.shp", "test_output/selected.shp", '"id" >=10')
+    Qpy.select_vector("test_data/multiple_polygons.shp", "test_output/selected.shp", '"id" >=10')
     print("Selection succeeded")
 except Exception:
     traceback.print_exc(file=sys.stdout)
 
 try:
     print("testing clip")
-    Qpy.Clip_analysis("test_data/multiple_polygons.shp", "test_data/multiple_polygons2.shp", "test_output/clipped.shp")
+    Qpy.clip_vector("test_data/multiple_polygons.shp", "test_data/multiple_polygons2.shp", "test_output/clipped.shp")
 except Exception:
     traceback.print_exc(file=sys.stdout)
 
 try:
     print("testing union")
-    Qpy.Union_analysis(["test_data/multiple_polygons.shp", "test_data/multiple_polygons2.shp"], "test_output/union.shp")
+    Qpy.union_vector(["test_data/multiple_polygons.shp", "test_data/multiple_polygons2.shp"], "test_output/union.shp")
 except Exception:
     traceback.print_exc(file=sys.stdout)
 
